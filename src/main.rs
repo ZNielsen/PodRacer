33246b67fc21448f2acbd3581d1de1eb1c92ae1e
Zach Nielsen <zach@zachn.me>
2020-11-03 16:43:32 -0800

    Add sample output stuff

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
 fn main() {
-    println!("Hello, world!");
+    println!("What's the URL?: ");
+    println!("There are {} episodes, and you are {} behind.", 420, 69);
+    println!("How would you like to catch up?");
+    println!("1) Catch up by certain date");
+    println!("2) Consume episodes at an accelerated rate");
+    println!("Choose 1/2: ");
 }
