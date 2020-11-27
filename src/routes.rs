83df05fc3f10d6593e4c6be4c3065d655027d32e
Zach Nielsen <zach@zachn.me>
2020-11-27 09:42:35 -0800

    Correct spacing

--- src/routes.rs
+++ src/routes.rs
@@ -132,94 +132,99 @@ pub fn create_feed_handler( config: State<RocketConfig>,
  //     integrate_new -
  // RETURN: A result with string information either way. Tailored for a curl response
  //
- #[post("/create_feed_cli?<url>&<rate>&<integrate_new>", rank = 2)]
- pub fn create_feed_cli_handler( config: State<RocketConfig>,
-                         url: String,
-                         rate: f32,
-                         integrate_new: bool
-                     ) -> Result<String, String> {
-     match create_feed( racer::RacerCreationParams {
-         address: config.address.clone(),
-         port: config.port,
-         url: url,
-         rate:rate,
-         integrate_new: integrate_new,
-         start_ep: 1
-     }) {
-         Ok(val) => Ok(make_fun_fact_string_cli(&val)),
-         Err(e) => Err(e)
-     }
- }
-
- ////////////////////////////////////////////////////////////////////////////////
-  // NAME:   create_feed_cli_ep_handler
-  //
-  // NOTES:
-  //     Creatse a new PodRacer feed, but includes a start episode. This is
-  //     probably from the form.
-  // ARGS:
-  //     config -
-  //     url -
-  //     rate -
-  //     integrate_new -
-  //     start_ep -
-  // RETURN:
-  //     A result containing either a success file or a failure file.
-  //     If Ok(), the File will have the subscribe url to display to the user
-  //
- #[post("/create_feed_cli?<url>&<rate>&<integrate_new>&<start_ep>", rank = 1)]
- pub fn create_feed_cli_ep_handler(config: State<RocketConfig>,
-                           url: String,
-                           rate: f32,
-                           integrate_new: bool,
-                           start_ep: usize
-                         ) -> Result<String, String> {
-     match create_feed( racer::RacerCreationParams {
-         address: config.address.clone(),
-         port: config.port,
-         url: url,
-         rate:rate,
-         integrate_new: integrate_new,
-         start_ep: start_ep
-     }) {
+#[post("/create_feed_cli?<url>&<rate>&<integrate_new>", rank = 2)]
+pub fn create_feed_cli_handler( config: State<RocketConfig>,
+                                url: String,
+                                rate: f32,
+                                integrate_new: bool
+) -> Result<String, String> {
+    match create_feed( racer::RacerCreationParams {
+        address: config.address.clone(),
+        port: config.port,
+        url: url,
+        rate:rate,
+        integrate_new: integrate_new,
+        start_ep: 1
+    }) {
         Ok(val) => Ok(make_fun_fact_string_cli(&val)),
         Err(e) => Err(e)
-     }
- }
+    }
+}
 
- ////////////////////////////////////////////////////////////////////////////////
-  // NAME:   create_feed
-  //
-  // NOTES:
-  //     Crates a PodRacer feed for given parameters. Prints some stats for the
-  //     user for display over curl. Might need to make another version to handle
-  //     displaying info over web UI
-  // ARGS:   params - All the parameters required to put together a feed. See
-  //                  the struct for more info.
-  // RETURN:
-  //     A result. If Ok(), contains a bunch of stats for the user. If Err(),
-  //     contains info for why it failed
-  //
- fn create_feed(mut params: racer::RacerCreationParams) -> Result<FeedFunFacts,String> {
-     let feed_racer = match racer::create_feed(&mut params) {
-         Ok(val) => val,
-         Err(e) => return Err(e),
-     };
-     println!("{}", feed_racer);
+////////////////////////////////////////////////////////////////////////////////
+ // NAME:   create_feed_cli_ep_handler
+ //
+ // NOTES:
+ //     Creatse a new PodRacer feed, but includes a start episode. This is
+ //     probably from the form.
+ // ARGS:
+ //     config -
+ //     url -
+ //     rate -
+ //     integrate_new -
+ //     start_ep -
+ // RETURN:
+ //     A result containing either a success file or a failure file.
+ //     If Ok(), the File will have the subscribe url to display to the user
+ //
+#[post("/create_feed_cli?<url>&<rate>&<integrate_new>&<start_ep>", rank = 1)]
+ pub fn create_feed_cli_ep_handler( config: State<RocketConfig>,
+                                    url: String,
+                                    rate: f32,
+                                    integrate_new: bool,
+                                    start_ep: usize
+) -> Result<String, String> {
+    match create_feed( racer::RacerCreationParams {
+        address: config.address.clone(),
+        port: config.port,
+        url: url,
+        rate:rate,
+        integrate_new: integrate_new,
+        start_ep: start_ep
+    }) {
+       Ok(val) => Ok(make_fun_fact_string_cli(&val)),
+       Err(e) => Err(e)
+    }
+}
 
-     // Grab some info to return
-     let path: PathBuf = [feed_racer.get_racer_path().to_str().unwrap(), racer::ORIGINAL_RSS_FILE].iter().collect();
-     super::scrub_xml(&path);
-     println!("Getting file from {}", path.display());
-     let file = File::open(&path).unwrap();
-     let mut buf = std::io::BufReader::new(&file);
-     let feed = rss::Channel::read_from(&mut buf).unwrap();
-     let num_items = feed.items().len() - &params.start_ep;
-     let weeks_behind = feed_racer.get_first_pubdate().signed_duration_since(chrono::Utc::now()).num_weeks().abs();
-     let weeks_to_catch_up = ((weeks_behind as f32) / feed_racer.get_rate()) as u32;
-     let days_to_catch_up = (((weeks_behind*7) as f32) / feed_racer.get_rate()) as u32;
-     let catch_up_date = chrono::Utc::now() + chrono::Duration::weeks(weeks_to_catch_up as i64);
 
+
+ //
+ // Helper Functions
+ //
+
+////////////////////////////////////////////////////////////////////////////////
+ // NAME:   create_feed
+ //
+ // NOTES:
+ //     Crates a PodRacer feed for given parameters. Prints some stats for the
+ //     user for display over curl. Might need to make another version to handle
+ //     displaying info over web UI
+ // ARGS:   params - All the parameters required to put together a feed. See
+ //                  the struct for more info.
+ // RETURN:
+ //     A result. If Ok(), contains a bunch of stats for the user. If Err(),
+ //     contains info for why it failed
+ //
+fn create_feed(mut params: racer::RacerCreationParams) -> Result<FeedFunFacts,String> {
+    let feed_racer = match racer::create_feed(&mut params) {
+        Ok(val) => val,
+        Err(e) => return Err(e),
+    };
+    println!("{}", feed_racer);
+
+    // Grab some info to return
+    let path: PathBuf = [feed_racer.get_racer_path().to_str().unwrap(), racer::ORIGINAL_RSS_FILE].iter().collect();
+    super::scrub_xml(&path);
+   println!("Getting file from {}", path.display());
+   let file = File::open(&path).unwrap();
+   let mut buf = std::io::BufReader::new(&file);
+   let feed = rss::Channel::read_from(&mut buf).unwrap();
+   let num_items = feed.items().len() - &params.start_ep;
+   let weeks_behind = feed_racer.get_first_pubdate().signed_duration_since(chrono::Utc::now()).num_weeks().abs();
+   let weeks_to_catch_up = ((weeks_behind as f32) / feed_racer.get_rate()) as u32;
+   let days_to_catch_up = (((weeks_behind*7) as f32) / feed_racer.get_rate()) as u32;
+   let catch_up_date = chrono::Utc::now() + chrono::Duration::weeks(weeks_to_catch_up as i64);
 
     Ok( FeedFunFacts {
         num_items: num_items,
@@ -241,127 +246,124 @@ fn make_fun_fact_string_cli(fff: &FeedFunFacts) -> String {
     ret
 }
 
- ////////////////////////////////////////////////////////////////////////////////
-  // NAME:   update_one_handler
-  //
-  // NOTES:  Update one podcast, specified by folder name or subscribe url
-  // ARGS:
-  //     podcast - The podcast to update. Specified by the folder name or
-  //               the PodRacer subscribe url.
-  // RETURN:
-  //
- #[post("/update/<podcast>")]
- pub fn update_one_handler(podcast: String) -> std::io::Result<()> {
-     // Update the specified podcast
-     // Check if podcast is folder name
-     if let Some(mut racer) = racer::get_by_dir_name(&podcast) {
-         return racer.update(&racer::RssFile::Download);
-     }
-     // Check if subscribe url
-     if let Some(mut racer) = racer::get_by_url(&podcast) {
-         return racer.update(&racer::RssFile::Download);
-     }
-     Err(Error::new(ErrorKind::NotFound, format!("podcast not found")))
- }
+////////////////////////////////////////////////////////////////////////////////
+ // NAME:   update_one_handler
+ //
+ // NOTES:  Update one podcast, specified by folder name or subscribe url
+ // ARGS:
+ //     podcast - The podcast to update. Specified by the folder name or
+ //               the PodRacer subscribe url.
+ // RETURN:
+ //
+#[post("/update/<podcast>")]
+pub fn update_one_handler(podcast: String) -> std::io::Result<()> {
+    // Update the specified podcast
+    // Check if podcast is folder name
+    if let Some(mut racer) = racer::get_by_dir_name(&podcast) {
+        return racer.update(&racer::RssFile::Download);
+    }
+    // Check if subscribe url
+    if let Some(mut racer) = racer::get_by_url(&podcast) {
+        return racer.update(&racer::RssFile::Download);
+    }
+    Err(Error::new(ErrorKind::NotFound, format!("podcast not found")))
+}
 
- ////////////////////////////////////////////////////////////////////////////////
-  // NAME:   update_all_handler
-  //
-  // NOTES:  Forces update of all podcast feeds on this server
-  // ARGS:   None
-  // RETURN: A result. If errored, a string containing some error info
-  //
- #[post("/update")]
- pub fn update_all_handler() -> Result<(), String> {
-     match racer::update_all() {
-         Ok(_) => Ok(()),
-         Err(string) => Err(format!("Error in update_all_handler: {}", string)),
-     }
- }
+////////////////////////////////////////////////////////////////////////////////
+ // NAME:   update_all_handler
+ //
+ // NOTES:  Forces update of all podcast feeds on this server
+ // ARGS:   None
+ // RETURN: A result. If errored, a string containing some error info
+ //
+#[post("/update")]
+pub fn update_all_handler() -> Result<(), String> {
+    match racer::update_all() {
+        Ok(_) => Ok(()),
+        Err(string) => Err(format!("Error in update_all_handler: {}", string)),
+    }
+}
+////////////////////////////////////////////////////////////////////////////////
+ // NAME:   delete_feed_handler
+ //
+ // NOTES:
+ //     Deletes the sepecified feed. No authentication required, so anyone can
+ //     get in there and cause havoc. Probs should change that.
+ // ARGS:   podcast - the podcast to delete. Can be a dir name or PodRacer URL
+ // RETURN: Result - strings with info either way
+ //
+ // Eventually want to expand this to be a button on the web UI after listing all podcasts
+//#[post("/delete_feed?<podcast>")]
+//pub fn delete_feed_handler(podcast: String) -> Result<String, String> {
+    //Err(format!("Need to put this behind some sort of auth so random people can't delete things"))
+    // // Try dir name first
+    // let mut dir = dirs::home_dir().unwrap();
+    // dir.push(racer::PODRACER_DIR);
+    // dir.push(&podcast);
+    // if dir.is_dir() {
+    //     // Delete it and return Ok
+    //     match std::fs::remove_dir_all(dir.as_path()) {
+    //         Ok(_) => return Ok(format!("Podcast deleted from server: {}", &podcast)),
+    //         Err(e) => {
+    //             println!("Error removing podcast {} from sever: {}", &podcast, e);
+    //             return Err(format!("Error removing podcast from server."));
+    //         },
+    //     };
+    // }
+    // Not a dir, search for a FeedRacer that has this URL
+    // Err(format!("TODO: search racers for this url: {}", podcast))
+//}
 
- ////////////////////////////////////////////////////////////////////////////////
-  // NAME:   delete_feed_handler
-  //
-  // NOTES:
-  //     Deletes the sepecified feed. No authentication required, so anyone can
-  //     get in there and cause havoc. Probs should change that.
-  // ARGS:   podcast - the podcast to delete. Can be a dir name or PodRacer URL
-  // RETURN: Result - strings with info either way
-  //
-  // Eventually want to expand this to be a button on the web UI after listing all podcasts
- //#[post("/delete_feed?<podcast>")]
- //pub fn delete_feed_handler(podcast: String) -> Result<String, String> {
-     //Err(format!("Need to put this behind some sort of auth so random people can't delete things"))
-     // // Try dir name first
-     // let mut dir = dirs::home_dir().unwrap();
-     // dir.push(racer::PODRACER_DIR);
-     // dir.push(&podcast);
-     // if dir.is_dir() {
-     //     // Delete it and return Ok
-     //     match std::fs::remove_dir_all(dir.as_path()) {
-     //         Ok(_) => return Ok(format!("Podcast deleted from server: {}", &podcast)),
-     //         Err(e) => {
-     //             println!("Error removing podcast {} from sever: {}", &podcast, e);
-     //             return Err(format!("Error removing podcast from server."));
-     //         },
-     //     };
-     // }
-
-     // Not a dir, search for a FeedRacer that has this URL
-     // Err(format!("TODO: search racers for this url: {}", podcast))
- //}
-
- ////////////////////////////////////////////////////////////////////////////////
-  // NAME:   list_feeds_handler
-  //
-  // NOTES:
-  //     List all the feeds on this server. Gives a list of podcasts by the
-  //     directory names: <podcast_name>_<rate>_<feed creation date>
-  // ARGS:   None
-  // RETURN: Result string - either the feeds or info on what failed
-  //
- #[get("/list_feeds")]
- pub fn list_feeds_handler() -> Result<String, String> {
-     let mut ret = String::new();
-     // Get all folders in the podracer dir
-     let podcast_dirs = match racer::get_all_podcast_dirs() {
-         Ok(val) => val,
-         Err(str) => {
-             println!("Error in list_feeds_handler: {}", str);
-             return Err(format!("Error getting feeds, check logs"));
-         },
-     };
-     for podcast_dir in podcast_dirs {
-         let path = match podcast_dir {
-             Ok(val) => val.path(),
-             Err(e) => return Err(format!("Error iterating over path from read_dir: {}", e)),
-         };
-         let this_dir_name = path.file_name().unwrap();
-         ret.push_str(this_dir_name.to_str().unwrap());
-         ret.push_str("\n");
-     }
-     Ok(ret)
- }
-
- ////////////////////////////////////////////////////////////////////////////////
-  // NAME:   serve_rss_handler
-  //
-  // NOTES:
-  //     Serves the racer.rss file for the specified podcast. This is almost
-  //     certainly called by a podcast player to check for new episodes.
-  //     TODO: Should we not serve the file if nothing changed? Is there a safe
-  //         way to do that?
-  // ARGS:   podcast - The podcast to serve. Format is the folder name
-  // RETURN: Our PodRacer RSS file
-  //
- #[get("/podcasts/<podcast>/racer.rss")]
- pub fn serve_rss_handler(podcast: String) -> Option<File> {
-     println!("Serving at {}", chrono::Utc::now().to_rfc3339());
-     // Serve the rss file
-     let home = dirs::home_dir()?;
-     let path: PathBuf = [home.to_str()?, racer::PODRACER_DIR, &podcast, racer::RACER_RSS_FILE].iter().collect();
-     match std::fs::File::open(path) {
-         Ok(file) => Some(file),
-         Err(_) => None,
-     }
- }
+////////////////////////////////////////////////////////////////////////////////
+ // NAME:   list_feeds_handler
+ //
+ // NOTES:
+ //     List all the feeds on this server. Gives a list of podcasts by the
+ //     directory names: <podcast_name>_<rate>_<feed creation date>
+ // ARGS:   None
+ // RETURN: Result string - either the feeds or info on what failed
+ //
+#[get("/list_feeds")]
+pub fn list_feeds_handler() -> Result<String, String> {
+    let mut ret = String::new();
+    // Get all folders in the podracer dir
+    let podcast_dirs = match racer::get_all_podcast_dirs() {
+        Ok(val) => val,
+        Err(str) => {
+            println!("Error in list_feeds_handler: {}", str);
+            return Err(format!("Error getting feeds, check logs"));
+        },
+    };
+    for podcast_dir in podcast_dirs {
+        let path = match podcast_dir {
+            Ok(val) => val.path(),
+            Err(e) => return Err(format!("Error iterating over path from read_dir: {}", e)),
+        };
+        let this_dir_name = path.file_name().unwrap();
+        ret.push_str(this_dir_name.to_str().unwrap());
+        ret.push_str("\n");
+    }
+    Ok(ret)
+}
+////////////////////////////////////////////////////////////////////////////////
+ // NAME:   serve_rss_handler
+ //
+ // NOTES:
+ //     Serves the racer.rss file for the specified podcast. This is almost
+ //     certainly called by a podcast player to check for new episodes.
+ //     TODO: Should we not serve the file if nothing changed? Is there a safe
+ //         way to do that?
+ // ARGS:   podcast - The podcast to serve. Format is the folder name
+ // RETURN: Our PodRacer RSS file
+ //
+#[get("/podcasts/<podcast>/racer.rss")]
+pub fn serve_rss_handler(podcast: String) -> Option<File> {
+    println!("Serving at {}", chrono::Utc::now().to_rfc3339());
+    // Serve the rss file
+    let home = dirs::home_dir()?;
+    let path: PathBuf = [home.to_str()?, racer::PODRACER_DIR, &podcast, racer::RACER_RSS_FILE].iter().collect();
+    match std::fs::File::open(path) {
+        Ok(file) => Some(file),
+        Err(_) => None,
+    }
+}
