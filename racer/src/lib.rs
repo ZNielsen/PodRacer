631c6524337fb09aab31562cec6f05b811de1ba7
Zach Nielsen <zach@zachn.me>
2020-12-06 05:07:31 -0800

    More description shenanigans

--- racer/src/lib.rs
+++ racer/src/lib.rs
@@ -165,10 +165,10 @@ impl FeedRacer {
         let next_pub_date_str = if items.len() > 0 {
             let next_item = self.release_dates[self.get_num_to_publish()].clone();
             let s = DateTime::parse_from_rfc2822(&next_item.date).unwrap().with_timezone(&Local).format("%d %b %Y at %I:%M %P");
-            format!("<p><br><br>Next episode publishes {}</p>", s)
+            format!("&amp;ltbr&amp;gtNext episode publishes {}", s)
         }
         else {
-            format!("<br><br><p>PodRacer feed has caught up.<br>The next episode will come out when creator publishes it.</p>")
+            format!("&amp;ltbr&amp;gtPodRacer feed has caught up.&amp;ltbr&amp;gtThe next episode will come out when creator publishes it.</p>")
         };
         rss.set_description(format!("{}{}", rss.description(), &next_pub_date_str));
 
