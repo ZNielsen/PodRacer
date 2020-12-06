940332e716f79c0a684afad0332ab12ab2e104b1
Zach Nielsen <zach@zachn.me>
2020-12-05 18:40:56 -0800

    Fix bad description

--- racer/src/lib.rs
+++ racer/src/lib.rs
@@ -168,12 +168,12 @@ impl FeedRacer {
             let s = DateTime::parse_from_rfc2822(&next_item.date).unwrap().with_timezone(&Local).format("%d %b %Y at %I:%M %P");
             // println!("next episode is {}", next_item.title);
             // println!("next pub date is {}, or {} UTC", s, &next_item.date);
-            format!("\n\nNext episode publishes {}", s)
+            format!("<br><br>Next episode publishes {}", s)
         }
         else {
-            format!("\n\nPodRacer feed has caught up.\nThe next episode will come out when creator publishes it.")
+            format!("<br><br>PodRacer feed has caught up.<br>The next episode will come out when creator publishes it.")
         };
-        rss.set_description(String::from(rss.description()) + &next_pub_date_str);
+        rss.set_description(format!("<![CDATA[{}{}]]>", rss.description(), &next_pub_date_str));
 
         // Append racer publish date to the end of the description
         for (item, info) in items_to_publish.iter_mut().zip(self.release_dates.iter()) {
